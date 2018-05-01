a9d9cadad712c6ac4201c211eb4da8ff626ef5d3
andriisvirskyi <andriisvirskyi@gmail.com>
2018-05-01 18:55:36 +0300

    add registration

--- config.js
+++ config.js
@@ -1,4 +1,10 @@
+const dotenv = require('dotenv');
+const path = require('path');
+const root = path.join.bind(this,__dirname);
+dotenv.config({path: root('.env')});
+
 module.exports = {
   PORT: process.env.PORT || 3000,
-  MONGO_URL: 'mongodb://localhost/blog'
+  MONGO_URL: process.env.MONGO_URL,
+  IS_PODUCTION: process.env.NODE_ENV === 'production'
 };
